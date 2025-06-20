0704afac389df6e8b67d14cdeefb44a1fcfb1413
CodeByMB <codebymb@gmail.com>
2025-06-20 20:54:07 +0200

    update

--- Tkinter_basics/sliders.py
+++ Tkinter_basics/sliders.py
@@ -1,5 +1,20 @@
 import tkinter as tk
 import ttkbootstrap as ttk
 
-window = ttk.Window()
-window.title()
+window = ttk.Window(themename = 'superhero')
+window.title("Sliders")
+window.resizable(False, False)
+window.geometry("600x400")
+
+scale_int = tk.IntVar(value = 15)
+scale = ttk.Scale(
+    window,
+    command= lambda value: print(value),
+    from_ = 0,
+    to = 25,
+    length = 300,
+    orient = tk.VERTICAL,
+    variable = scale_int,)
+scale.pack(padx=5, pady=5)
+
+window.mainloop()
