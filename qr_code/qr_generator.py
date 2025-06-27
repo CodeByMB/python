46d2576349d5a5b6f7d1d0ea8419c7b79e6d2c6c
CodeByMB <codebymb@gmail.com>
2025-06-27 20:26:11 +0200

    uploads

---
+++ qr_code/qr_generator.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+import customtkinter as ctk
+from PIL import Image, ImageTk
+
+class App(ctk.CTk):
+	def __init__(self):
+		ctk.set_appearance_mode('light')
+		self.overrideredirect(True)
+		super().__init__(fg_color= 'white')
+
+
+
+		self.mainloop()
+
+
+
+App()
+
+True
--- test.py
+++ test.py
@@ -1,34 +1,21 @@
-import tkinter as tk
-from tkinter import RIGHT, X, BOTH
+inventory = ['zwaard', 'schild', 'gezondheidsdrank']
 
-import ttkbootstrap as ttk
+def gebruik_item(item_naam):
+    for item in inventory:
+        if item == item_naam:
+            print(f'Je gebruikt de {item_naam}.')
+            inventory.remove(item_naam)
+        else:
+            print('Je hebt dit item niet.')
 
-# window
-root = ttk.Window(themename = 'superhero')
-root.geometry('600x400')
-root.title('Menu')
+item_naam = input("Welk item wil je gebruiken?: ")
+gebruik_item(item_naam)
+print(inventory)
 
-def move_window(event):
-    root.geometry('+{0}+{1}'.format(event.x_root, event.y_root))
+class Enemy:
+    def __init__(self, naam, health):
+        self.naam = naam
+        self.health = health
 
-root.overrideredirect(True) # turns off title bar, geometry
-root.geometry('400x100+200+200') # set new geometry
-
-# make a frame for the title bar
-title_bar = ttk.Frame(root, relief='raised')
-
-# put a close button on the title bar
-close_button = ttk.Button(title_bar, text='X', command=root.destroy)
-
-# a canvas for the main area of the window
-window = ttk.Canvas(root, bg='black')
-
-# pack the widgets
-title_bar.pack(expand=1, fill=X)
-close_button.pack(side=RIGHT)
-window.pack(expand=1, fill=BOTH)
-
-# bind title bar motion to the move window function
-title_bar.bind('<B1-Motion>', move_window)
-
-root.mainloop()
+    def take_damage(self, damage):
+        self.health -= damage
